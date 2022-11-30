014c3b44de895d17f359f6bc34043a46e91a76cd
Alessandro <alessandro.fd@gmail.com>
2022-11-29 21:21:43 -0300

    2022/11/29 - Trilha da Programação

---
+++ json_ex.js
@@ -0,0 +1,19 @@
+{
+    "emp_details": [
+        {
+            "emp_name": "Shubham",
+            "email": "ksingh.shubh@gmail.com",
+            "job_profile": "intern"
+        },
+        {
+            "emp_name": "Alessandro",
+            "email": "alessandro@gmail.com",
+            "job_profile": "developer"
+        },
+        {
+            "emp_name": "Tiago",
+            "email": "tiago@gmail.com",
+            "job_profile": "Full Time"
+        }
+    ]
+}
