770037b2f6148f51cd5dd6e9f43901f6cfbdc615
Alessandro <alessandro.fd@gmail.com>
2022-12-16 17:19:35 -0300

    2022/12/12 V.9 - Trilha da Programação Exercicios_coursera semana 4 - Digito Adjacente OK

--- primo.py
+++ primo.py
@@ -1,16 +1,30 @@
-num = int(input("Digite um número:"))
-temp = str(num)
-tamanho = len(temp)
-ultimo = 9
-primeiro = 99
-primo = 0
+numeroPrimo = 99
 
-while tamanho >= 0:
-    ultimo = num // 10
-    primeiro = num // 10
-    if(ultimo == primeiro):
-        tamanho = tamanho - 1
-        print(primeiro)
+def retornaPar(num):
+    if(num <= 2):
+        valor = num % 2 + num % 3 + num % 5 + num % 7 + num % 9
+    if(valor == 0 and valor != 11):
+        primo = 0
+    return(primo)
+
+def retornaComprimentoNumero(num):
+    num = int(input("Digite um número:"))
+    temp = str(num)
+    comprimentoNumero = len(temp)
+    return(comprimentoNumero)
+
+def verificaAdjacente(num, cn):
+    idx = cn
+    while(idx >= 1 and ultimoAlgarismo == proximoAlgarismo):
+        ultimoAlgarismo = num % 10
+        completo = num // 10
+        proximoAlgarismo = completo%10
+        idx = idx - 1
+        if(idx == 1)
+            primo = 0
+    return(primo)
+
+
 
 
 
