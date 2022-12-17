46c39a4111e418648bdd76b6f6eda9e9a2c92134
Alessandro <alessandro.fd@gmail.com>
2022-12-17 15:02:13 -0300

    2022/12/12 V.11 - Trilha da Programação Exercicios_coursera semana 4 - Fatorial e binomial - OK

---
+++ binomio.py
@@ -0,0 +1,23 @@
+
+
+n = int(input("Infome um número N : "))
+k = int(input("Infome um número K (Menor ou igual a N): "))
+if(k <= n):
+
+    def fatorial(n):
+
+        fat = 1
+        idx = 1
+
+        while(idx <= n):
+            fat = idx * fat
+            idx = idx + 1
+        return(fat)
+
+    def numeroBinomial(n, k):
+
+        return fatorial(n)//((fatorial(k) * (fatorial(n - k))))
+
+    b = numeroBinomial(n, k)
+    print(b)
+else: print("K precisa ser menor ou igual a N")
